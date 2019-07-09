be65d697f2b35d5e7ef0a4105578817e4c254a3c
Jacob Gronert <jacobgronert@gmail.com>
2019-07-09 00:25:03 -0700

    checkpoint, header halfway complete

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { Header } from './shared/Header'
+import "./../css/App.css";
+
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
+        <div className='background-overlay'></div>
+        <Header />
+        <img className='background-image' src="/images/deck.png" type="image" />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/shared/Header.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import "./../../css/header.css";
+
+export class Header extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      middle: true
+    }
+  }
+
+  reposition() {
+    this.setState({middle: !this.state.middle});
+  }
+
+  render(){
+    let position = this.state.middle ? "middle" : "top";
+
+    return(
+      <div className={"Header "+position} onClick={this.reposition.bind(this)}>
+        <h1 className='logo underline'> The Evernden Cabin </h1>
+        <ul className='navigation-list' >
+          <li>News</li>
+          <li>Photos</li>
+          <li>Calendar</li>
+        </ul>
+      </div>
+    );
+  }
+}
