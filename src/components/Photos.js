98491f4d4e506906fcf57587ab2f707ef3462552
Jacob Gronert <jacobgronert@gmail.com>
2019-11-24 21:54:38 -0800

    finished photos

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./../css/Photos.css";
+import Gallery from 'react-grid-gallery';
 
 export class Photos extends Component{
   constructor(props) {
@@ -7,12 +8,47 @@ export class Photos extends Component{
     this.state = {
       visibility: props.visibility,
       modalIsOpen: true,
-      photos: [
-                {src:'http://via.placeholder.com/400x400/', name:'1'},
-                {src:'http://via.placeholder.com/400x400/', name:'2'},
-                {src:'http://via.placeholder.com/600x400/', name:'3'},
-                {src:'http://via.placeholder.com/800x400/', name:'4'},
-              ]
+      photos: [{
+                src: "images/Uploaded_Photos/IMAG0310.jpg",
+                thumbnail: "images/Uploaded_Photos/IMAG0310.jpg",
+                caption: "After Rain"
+              },
+              {
+                src: "images/Uploaded_Photos/IMAG0316.jpg",
+                thumbnail: "images/Uploaded_Photos/IMAG0316.jpg",
+              },
+              {
+                src: "images/Uploaded_Photos/Kings Hole 1.jpg",
+                thumbnail: "images/Uploaded_Photos/Kings Hole 1.jpg",
+              },
+              {
+                src: "images/Uploaded_Photos/Kings Hole 2.jpg",
+                thumbnail: "images/Uploaded_Photos/Kings Hole 2.jpg",
+              },
+              {
+                src: "images/Uploaded_Photos/Kings Hole 3.jpg",
+                thumbnail: "images/Uploaded_Photos/Kings Hole 3.jpg",
+              },
+              {
+                src: "images/Uploaded_Photos/Kings Hole 4.jpg",
+                thumbnail: "images/Uploaded_Photos/Kings Hole 4.jpg",
+              },
+              {
+                src: "images/Uploaded_Photos/Kings Hole Sign.jpg",
+                thumbnail: "images/Uploaded_Photos/Kings Hole Sign.jpg",
+              },
+              {
+                src: "images/Uploaded_Photos/River.jpg",
+                thumbnail: "images/Uploaded_Photos/River.jpg",
+              },
+              {
+                src: "images/Uploaded_Photos/tracks.jpg",
+                thumbnail: "images/Uploaded_Photos/tracks.jpg",
+              },
+              {
+                src: "images/Uploaded_Photos/Tree.jpg",
+                thumbnail: "images/Uploaded_Photos/Tree.jpg",
+              }]
     }
   }
 
@@ -27,9 +63,11 @@ export class Photos extends Component{
       <div className={"Photos "+visibility}>
         <h2>Photos</h2>
         <div className="photo-grid">
-          { this.state.photos.map((photo, key) => 
+          {/* { this.state.photos.map((photo, key) => 
             <img key={photo.name} src={photo.src} alt="cabin" />
-          )}
+          )} */}
+
+          <Gallery images={this.state.photos}/>
         </div>
       </div>
     );
