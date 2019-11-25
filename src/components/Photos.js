7e72e3895f7a8a0c0612433b923be537a0d0db25
Jacob Gronert <jacobgronert@gmail.com>
2019-11-24 20:58:55 -0800

    small commit wip

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import ReactPhotoGrid from 'react-photo-grid';
 import "./../css/Photos.css";
 
 export class Photos extends Component{
@@ -9,8 +8,10 @@ export class Photos extends Component{
       visibility: props.visibility,
       modalIsOpen: true,
       photos: [
-                {src:'http://via.placeholder.com/400x400/', name:'1'}, 
-                {src:'http://via.placeholder.com/500x700/', name:'2'}
+                {src:'http://via.placeholder.com/400x400/', name:'1'},
+                {src:'http://via.placeholder.com/400x400/', name:'2'},
+                {src:'http://via.placeholder.com/600x400/', name:'3'},
+                {src:'http://via.placeholder.com/800x400/', name:'4'},
               ]
     }
   }
@@ -21,24 +22,14 @@ export class Photos extends Component{
 
   render(){
     let visibility = this.state.visibility ? "visible" : "hidden";
-    var imageData = [
-        'http://via.placeholder.com/400x400/',
-        'http://via.placeholder.com/500x700/',
-        'http://via.placeholder.com/600x500/',
-        'http://via.placeholder.com/600x800/'
-    ];
 
     return(
       <div className={"Photos "+visibility}>
         <h2>Photos</h2>
         <div className="photo-grid">
-          {/* { this.state.photos.map((photo, key) => 
+          { this.state.photos.map((photo, key) => 
             <img key={photo.name} src={photo.src} alt="cabin" />
-          )} */}
-
-          <ReactPhotoGrid
-            onImageClick={this.handleImageClick}
-            data={imageData} />
+          )}
         </div>
       </div>
     );
