0f06f6ebb914465e9eaa2bd9419bd2debef42750
Jacob Gronert <jacobgronert@gmail.com>
2019-11-25 19:58:03 -0800

    changed port for heroku

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,42 @@
 const path = require("path");
 const webpack = require("webpack");
 
-module.exports = {
-  entry: "./src/index.js",
-  mode: "development",
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: "babel-loader",
-        options: { presets: ["@babel/env"] }
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      },
-      {
-        test: /\.scss$/,
-        use: ["style-loader", "css-loader", 'sass-loader']
-      }
-    ]
-  },
-  resolve: { extensions: ["*", ".js", ".jsx"] },
-  output: {
-    path: path.resolve(__dirname, "dist/"),
-    publicPath: "/dist/",
-    filename: "bundle.js"
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "public/"),
-    port: process.env.PORT || 3000,
-    publicPath: "http://localhost:3000/dist/",
-    hotOnly: true,
-    historyApiFallback: true
-  },
-  plugins: [new webpack.HotModuleReplacementPlugin()]
+module.exports = () => {
+  var port = process.env.PORT || 3000;
+  return {
+    entry: "./src/index.js",
+    mode: "development",
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /(node_modules|bower_components)/,
+          loader: "babel-loader",
+          options: { presets: ["@babel/env"] }
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"]
+        },
+        {
+          test: /\.scss$/,
+          use: ["style-loader", "css-loader", 'sass-loader']
+        }
+      ]
+    },
+    resolve: { extensions: ["*", ".js", ".jsx"] },
+    output: {
+      path: path.resolve(__dirname, "dist/"),
+      publicPath: "/dist/",
+      filename: "bundle.js"
+    },
+    devServer: {
+      contentBase: path.join(__dirname, "public/"),
+      port: port,
+      publicPath: "http://localhost:3000/dist/",
+      hotOnly: true,
+      historyApiFallback: true
+    },
+    plugins: [new webpack.HotModuleReplacementPlugin()]
+  };
 };
