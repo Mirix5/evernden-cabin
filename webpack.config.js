c546eb7b0d7d330dd6769b931da7b4fa6b4acd33
Jacob Gronert <jacobgronert@gmail.com>
2019-11-25 21:39:13 -0800

    added webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,7 @@ module.exports = () => {
     resolve: { extensions: ["*", ".js", ".jsx"] },
     output: {
       path: path.resolve(__dirname, "dist/"),
+      globalObject: "this",
       publicPath: "/dist/",
       filename: "bundle.js"
     },
